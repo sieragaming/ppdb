4e796058e54f0a15b89f7d7ffa49af203f1a8a9d
sieragaming <sieragamming22@gmail.com>
2022-07-18 01:46:02 +0700

    make it better

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    "BASE_URL": "http://localhost:3000",
+    "BASE_URL": "https://next-ppdb.herokuapp.com",
     "MONGO_URL": "mongodb+srv://admin:admin@cluster0.rtcgt.mongodb.net/db_ppdb?retryWrites=true&w=majority",
     "ACCESS_TOKEN": "gerena123456",
     "REFRESH_TOKEN": "gerena121345689",
